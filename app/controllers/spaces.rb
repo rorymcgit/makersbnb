4aa72405d5c2bf3bef745a1b6091f3f47a002b77
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-03-21 12:33:43 +0000

    Add creation of spaces

--- app/controllers/spaces.rb
+++ app/controllers/spaces.rb
@@ -5,4 +5,18 @@ class MakersBnB < Sinatra::Base
     erb :spaces
   end
 
+  get '/spaces/new' do
+
+    erb :new
+  end
+
+  post '/spaces/new' do
+    Space.create(name: params[:name],
+    description: params[:description],
+    price: params[:price],
+    from_date: params[:from_date],
+    to_date: params[:to_date])
+    redirect '/spaces'
+  end
+
 end

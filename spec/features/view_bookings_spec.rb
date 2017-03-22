69ee18d7652f59c070ce0a5837fab86f546162c4
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-03-22 12:58:18 +0000

    Add view all bookings page

---
+++ spec/features/view_bookings_spec.rb
@@ -0,0 +1,23 @@
+feature "View bookings" do
+  scenario "view bookings logged in user has made" do
+    sign_up
+    create_space
+    sign_up(
+      full_name: "Booker",
+      username: "Booking_guy",
+      email: "wannabook@hotmail.com",
+      password: "abc",
+      password_confirmation: "abc"
+      )
+    visit('/spaces')
+    click_button("Book")
+    expect(current_path).to eq('/bookings')
+    expect(page).to have_content("Here are your bookings, Booker")
+    expect(page).to have_content("Name: Party House")
+    expect(page).to have_content("Description: Big house, 07544502382")
+    expect(page).to have_content("Price per night: £9.99")
+    expect(page).to have_content("Requested from: 05 July 2017")
+    expect(page).to have_content("Requested to: 10 July 2017")
+    expect(page).to have_content("Confirmation status: false")
+  end
+end

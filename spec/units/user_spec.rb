262beff0979b2d93af2b987be3be1c59951c7653
John Chang <johnchang9094@gmail.com>
2017-03-23 10:41:32 +0000

    Update user_spec.rb

--- spec/units/user_spec.rb
+++ spec/units/user_spec.rb
@@ -4,19 +4,19 @@ describe User do
     User.create(full_name: 'test', username: 'test', email: 'test@test.com', password: '12345', password_confirmation: '12345')
   end
 
-feature "user authentication" do
-  scenario "it authenticates when given valid email and password" do
+describe "user authentication" do
+  it "it authenticates when given valid email and password" do
     # sign_up
     authenticated_user = User.authenticate(test_user.email, test_user.password)
     expect(authenticated_user).to eq test_user
   end
 
-  scenario "it does not authenticate when given invalid parameters" do
+  it "it does not authenticate when given invalid parameters" do
     expect(User.authenticate(test_user.email, 'bad password')).to be_nil
   end
 end
 
-  scenario 'It does not create a user with an invalid email' do 
+  it 'It does not create a user with an invalid email' do 
   expect{User.create(full_name: 'test', username: 'test', email: 'testtest.com',
    password: '12345', password_confirmation: '12345')}.not_to change(User, :count)
 end 
